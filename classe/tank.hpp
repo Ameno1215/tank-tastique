522144a255620805aa11279beb9d751185faac27
censier <antonincensier@gmail.com>
2025-02-17 09:58:53 +0100

    premiere classe

--- classe/tank.hpp
+++ classe/tank.hpp
@@ -1,1 +1,21 @@
-int test;
+#ifndef TANK_H
+#define TANK_H
+
+#include "../config/config.hpp"
+
+class tank{
+    public :
+        int get_x();
+        int get_y();
+        int get_ori();
+        int get_vit();
+        
+    private :
+        int x;
+        int y;
+        double orientation;
+        double vitesse;
+}
+
+#endif
+
---
+++ config/config.hpp
@@ -0,0 +1,13 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <string>
+#include <stdlib>
+#include <unordered_map>
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#endif // CONFIG_HPP
